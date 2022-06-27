5b7ead4796edf2b594fdd75440baafbec3e08173
Michael Roni <89337191+micharlMR@users.noreply.github.com>
2022-06-27 16:03:35 +0700

    Feat: set route and navbar
    set route and navbar. The navbar is outside of route to make it look SPA

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+/** @jsxImportSource @emotion/react */
+
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import AnimeListPage from "./animeListPage";
+import AnimeDetailPage from "./animeDetailPage";
+import CollectionListPage from "./collectionListPage";
+
+function HomeNavbar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="/">Anime list</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Anime List</Nav.Link>
+            <Nav.Link href="/collections">Collections</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomeNavbar />
+      <Container>
+        <Router>
+          <Routes>
+            <Route path="/" element={<AnimeListPage />} />
+            <Route path="/details" element={<AnimeDetailPage />} />
+            <Route path="/collections" element={<CollectionListPage />} />
+          </Routes>
+        </Router>
+      </Container>
     </div>
   );
 }
