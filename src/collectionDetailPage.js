2ac9f2f6da7d58e21bf043a51a5b9c00d429aa26
Michael Roni <89337191+micharlMR@users.noreply.github.com>
2022-06-29 02:39:21 +0700

    Feat: add edit name on collection detail
    add edit collection name on collection detail page

--- src/collectionDetailPage.js
+++ src/collectionDetailPage.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useQuery, gql } from "@apollo/client";
-import { Col, Row, Button } from "react-bootstrap";
+import { Col, Row, Button, Modal, Form } from "react-bootstrap";
 import facepaint from "facepaint";
 
 /** @jsxImportSource @emotion/react */
@@ -34,11 +34,19 @@ const REQLISTANIME = gql`
 `;
 
 function CollectionDetailPage() {
+  const [collectionName, setCollectionName] = useState("");
   const [collection, setCollection] = useState("");
   const [listAnime, setListAnime] = useState([]);
 
   const location = useLocation();
   const { id } = location.state;
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShow = () => setShowModal(true);
+  const handleClose = () => setShowModal(false);
+
+  const onInput = ({ target: { value } }) => setCollectionName(value);
 
   useEffect(() => {
     let temp = [];
@@ -50,6 +58,22 @@ function CollectionDetailPage() {
     console.log(collection);
     console.log(listAnime);
   }, []);
+
+  const openEditDialog = () => {
+    console.log(showModal);
+    var temp = JSON.parse(localStorage.getItem("collections"));
+    setCollectionName(temp[id].collectionName);
+    handleShow();
+  };
+
+  const saveCollections = () => {
+    var arrayOfCollections = JSON.parse(localStorage.getItem("collections"));
+    console.log(arrayOfCollections);
+    arrayOfCollections[id].collectionName = collectionName;
+    localStorage.setItem("collections", JSON.stringify(arrayOfCollections));
+    setCollection(arrayOfCollections[id]);
+    setShowModal(false);
+  };
 
   console.log("ini list Anime: " + listAnime);
 
@@ -121,9 +145,45 @@ function CollectionDetailPage() {
             Collection : {collection.collectionName}
           </h1>
         </Col>
-        <Col></Col>
+        <Col>
+          <Button
+            css={mq({
+              float: "right",
+            })}
+            variant="success"
+            onClick={() => openEditDialog()}
+          >
+            Edit
+          </Button>
+        </Col>
       </Row>
-      {listAnime.length > 0 ? <CollectionAnimeList /> : ""}
+      {listAnime.length > 0 ? (
+        <CollectionAnimeList />
+      ) : (
+        <p>Currently there is no anime in this collection</p>
+      )}
+
+      <Modal show={showModal} onHide={handleClose} keyboard={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create Collections</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Collection Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Insert your collection name"
+                value={collectionName}
+                onChange={onInput}
+              />
+            </Form.Group>
+            <Button variant="primary" onClick={saveCollections}>
+              Save
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
